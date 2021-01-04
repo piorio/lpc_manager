48614a2e07a6fd83f47d3e24152218d6eb8bebe3
Paolo Iorio <piorio@soldo.com>
2021-01-04 18:10:48 +0100

    Create user The UI change based on the logged user seed the DB with default admin

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,18 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+# Add default Admin user
+case LpcManager.Users.create_admin(%{
+  email: "admin@example.com",
+  password: "password",
+  confirm_password: "password",
+  password_hash: Pow.Ecto.Schema.Password.pbkdf2_hash("password"),
+  role: "admin",
+  coach_name: "admin"
+}) do
+  {:ok, _user} -> IO.puts("Admin created successfully")
+  {:error, error_data} ->
+    IO.puts("Unable to create default admin user")
+    IO.inspect(error_data)
+end
